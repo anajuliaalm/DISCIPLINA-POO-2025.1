0e244dc33af6e11afc3dff01c8ccfce8718eccf5
Ana Júlia Almeida <anajulia@aluno.ifce.edu.br>
2025-05-06 22:22:43 -0300

    Add files via upload

--- LISTA-02/CAP-04/EXE-PROPOSTOS/Q07/Q07/src/br/edu/principal/Principal.java
+++ LISTA-02/CAP-04/EXE-PROPOSTOS/Q07/Q07/src/br/edu/principal/Principal.java
@@ -3,21 +3,32 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-    Scanner Scanner = new Scanner (System.in);
-    
-    
-    System.out.print("DIGITE O PESO DA PESSOA (em kg): ");
-    double peso = Scanner.nextDouble();
-    
-    double pesoEngordar = peso * 1.15;
-    
-        double pesoEmagrecer = peso * 0.80;
-    
-    
-    System.out.println("\nSE A PESSOA ENGORDAR 15%: " + String.format("%.2f", pesoEngordar) + " kg");
-    System.out.println("SE A PESSOA EMAGRECER 20%: " + String.format("%.2f", pesoEmagrecer) + " kg");
-    
-    
-    Scanner.close();
-}
+		Scanner s = new Scanner(System.in);
+
+        int mais50 = 0, entre10e20 = 0, pesoMenor40 = 0;
+        double somaAlturas = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int idade = s.nextInt();
+            double altura = s.nextDouble();
+            double peso = s.nextDouble();
+
+            if (idade > 50) mais50++;
+            if (idade >= 10 && idade <= 20) {
+                somaAlturas += altura;
+                entre10e20++;
+            }
+            if (peso < 40) pesoMenor40++;
+        }
+
+        System.out.println("Mais de 50 anos: " + mais50);
+        System.out.println("Média altura (10 a 20 anos): " + 
+            (entre10e20 > 0 ? somaAlturas / entre10e20 : 0));
+        System.out.println("Peso < 40kg: " + (pesoMenor40 * 100 / 5.0) + "%");
+
+        s.close();
+
+
+	}
+
 }
