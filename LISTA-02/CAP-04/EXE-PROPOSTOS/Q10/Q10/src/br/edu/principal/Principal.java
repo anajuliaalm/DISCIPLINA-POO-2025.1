ef61bf6addc49fe172f0955ef519083942c5f036
Ana Júlia Almeida <anajulia@aluno.ifce.edu.br>
2025-05-06 22:24:56 -0300

    Add files via upload

--- LISTA-02/CAP-04/EXE-PROPOSTOS/Q10/Q10/src/br/edu/principal/Principal.java
+++ LISTA-02/CAP-04/EXE-PROPOSTOS/Q10/Q10/src/br/edu/principal/Principal.java
@@ -3,18 +3,40 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-    Scanner Scanner = new Scanner(System.in);
-    
-    System.out.print("DIGITE O VALOR DO LADO DO QUADRADO: ");
-    double lado = Scanner.nextDouble();
+		 Scanner scanner = new Scanner(System.in);
 
-    
-    double area = lado * lado;
+	        int somaPares = 0;
+	        int somaPrimos = 0;
 
-    
-    System.out.println("A ÁREA DO QUADRADO É: " + String.format("%.2f", area));
+	        System.out.println("Digite 10 números:");
 
-    
-    Scanner.close();
-}
+	        for (int i = 0; i < 10; i++) {
+	            int numero = scanner.nextInt();
+
+	            if (numero % 2 == 0) {
+	                somaPares += numero;
+	            }
+
+	            if (ehPrimo(numero)) {
+	                somaPrimos += numero;
+	            }
+	        }
+
+	        System.out.println("Soma dos números pares: " + somaPares);
+	        System.out.println("Soma dos números primos: " + somaPrimos);
+	        
+	        scanner.close();
+	    }
+
+	    public static boolean ehPrimo(int n) {
+	        if (n <= 1) return false;
+	        if (n == 2) return true;
+	        if (n % 2 == 0) return false;
+
+	        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+	            if (n % i == 0) return false;
+	        }
+	        return true;
+	}
+
 }
