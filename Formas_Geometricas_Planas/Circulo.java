98ff633b5a7bb892301d52fa5fdc71b2d3bb50d3
Ana Júlia Almeida <anajulia@aluno.ifce.edu.br>
2025-05-16 08:56:57 -0300

    Add files via upload

---
+++ Formas_Geometricas_Planas/Circulo.java
@@ -0,0 +1,42 @@
+package br.edu.formasGeometricasplanas;
+
+public class Circulo {
+
+	
+	// Atributos 
+		private double raio;
+		private double area;
+		
+		// Metodos 
+		public Circulo () {
+			raio = 0;
+			area = 0; 
+		}
+		
+		public void recebeRaio (double pRaio) {
+			raio = pRaio;
+			
+		}
+		
+		public void calcArea () {
+			area = Math.PI * (raio * raio);
+		}
+		
+	   public void mostrarArea () {
+		   System.out.println(area);
+	   }
+		
+	   public double mostrarAreaComRetorno () {
+			System.out.println("A area do circulo é: " + area);
+			  return area;
+		  }
+			
+		
+		}
+
+
+
+		
+	
+
+
